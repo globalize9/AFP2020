2b5ae50db9240e8c70e608015a227978711d9a29
Yushi Wei <58374480+globalize9@users.noreply.github.com>
2020-09-07 19:18:37 -0700

    factors_cleaning_codes

---
+++ factors_cleaning.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+import os
+
+# set path
+os.chdir(r'C:\Users\yushi\Downloads\AFP2020-master\AFP2020-master')
+
+# read in macro and feedstock factors
+macro_factors_raw = pd.read_excel('BBGFactors.xlsx', sheet_name = 'macroFactors')
+feedstock_factors_raw = pd.read_excel('BBGFactors.xlsx', sheet_name = 'feedstockFactors')
+
+def CleanFactors(df, cut_off_date):
+    # preps the df for further analysis, specify date in this format '2020-08-20'
+    df.index = df.Dates
+    date_loc = np.where(df.index == cut_off_date)[0]
+    if len(date_loc) == 0: return 'Invalid date'
+    date_loc = date_loc[0]
+    df = df.loc[df.index[:date_loc]]
+    return df
+
+cut_off_date = '2020-08-20'
+macro_factors = CleanFactors(macro_factors_raw, cut_off_date)
+feedstock_factors = CleanFactors(feedstock_factors_raw, cut_off_date)
