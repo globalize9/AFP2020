52b40ebf9bd0b9a93b6aa844ef188d226a6d8fa1
Yushi Wei <58374480+globalize9@users.noreply.github.com>
2020-10-31 20:28:42 -0700

    updated data cleaning process

--- factors_cleaning.py
+++ factors_cleaning.py
@@ -7,6 +7,10 @@ This is a temporary script file.
 
 import pandas as pd
 import numpy as np
+import os
+
+# set path
+os.chdir(r'C:\Users\yushi\Documents\GitHub\AFP2020')
 
 # read in macro and feedstock factors
 macro_factors_raw = pd.read_excel('BBGFactors.xlsx', sheet_name = 'macroFactors')
@@ -27,4 +31,21 @@ def CleanFactors(df, cut_off_date):
 cut_off_date = '2019-12-31'
 macro_factors = CleanFactors(macro_factors_raw, cut_off_date)
 feedstock_factors = CleanFactors(feedstock_factors_raw, cut_off_date)
+
+# YoY adjustment
+def YoYClean(macro_factors):
+    macro_names = macro_factors.columns.tolist()
+    pct_yoy = ['YoY' in x for x in macro_names]
+    macro_factors_adj = pd.DataFrame(np.NaN, index = macro_factors.index, columns = macro_names)
+    for i in range(len(macro_names)):
+        if pct_yoy[i] == True:
+            macro_factors_adj.iloc[:,i] = (macro_factors.iloc[:,i] / macro_factors.iloc[:,i].shift(252) - 1) * 100
+        else:
+            macro_factors_adj.iloc[:,i] = macro_factors.iloc[:,i]
+    
+    macro_factors_adj = macro_factors_adj.dropna(axis = 0) # drop observations instead of columns this time
+    return macro_factors_adj
+
+macro_factors_adj = YoYClean(macro_factors)
+feedstock_factors_adj = YoYClean(feedstock_factors)
 
